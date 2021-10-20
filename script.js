43ca89a00089ff5c94cce18696ba03b8e18bfa26
Omar <morenoomar29@yahoo.com>
2021-10-19 19:22:32 -0500

    Adding images to the site

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
+/*
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+h2.textContent = "Software Engineer 2022";
+document.querySelector("h1").appendChild(h2);
+*/
+
+const divAdd = document.createElement("div");
+const bio = document.createElement("h2");
+bio.innerText="Background";
+divAdd.append(bio);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
